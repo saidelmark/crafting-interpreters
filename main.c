0e2e8edfd321bee5a1175eb43a4d6e5876ef040d
Mark Saidelman <mark.saidelman@akvelon.com>
2022-02-09 01:06:32 +0300

    Add list manipulations to main
    Basically it's a test of all the functions introduces so far.

--- main.c
+++ main.c
@@ -2,5 +2,16 @@
 #include "list.c"
 
 int main(int count, char *argv[]) {
+	push(1);
+	push(2);
+	push(3);
+	append(5);
+	printList();
+	printRevList();
+	pop();
+	trim();
+	printList();
+	destroy();
+	printList();
 	return 0;
 }
