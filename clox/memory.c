195c75b83acd01af8f7550db30910dfefa2a1bdc
Mark Saidelman <mark.saidelman@akvelon.com>
2023-01-24 18:29:35 +0400

    Add string type

--- clox/memory.c
+++ clox/memory.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+
 #include "memory.h"
+#include "vm.h"
 
 void* reallocate(void* pointer, size_t oldsize, size_t newSize) {
 	if (newSize == 0) {
@@ -10,4 +12,24 @@ void* reallocate(void* pointer, size_t oldsize, size_t newSize) {
 	void* result = realloc(pointer, newSize);
 	if (result == NULL) exit(1);
 	return result;
+}
+
+void freeObject(Obj* object) {
+	switch (object->type) {
+		case OBJ_STRING: {
+			ObjString* string = (ObjString*) object;
+			FREE_ARRAY(char, string->chars, string->length + 1);
+			FREE(OBJ_STRING, object);
+			break;
+		}
+	}
+}
+
+void freeObjects() {
+	Obj* object = vm.objects;
+	while (object != NULL) {
+		Obj* next = object->next;
+		freeObject(object);
+		object = next;
+	}
 }
