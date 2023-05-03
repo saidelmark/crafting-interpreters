97ecc7ee789ccd7a15b8a0af2963688f59fbc191
Mark Saidelman <mark.saidelman@akvelon.com>
2023-05-03 17:43:17 +0200

    Add loops

--- clox/vm.c
+++ clox/vm.c
@@ -197,6 +197,11 @@ static InterpretResults run() {
 				if (isFalsey(peek(0))) vm.ip += offset;
 				break;
 			}
+			case OP_LOOP: {
+				uint16_t offset = READ_SHORT();
+				vm.ip -= offset;
+				break;
+			}
 			case OP_RETURN:
 				// Do nothing for now, just exit interpreter
 				return INTERPRET_OK;
