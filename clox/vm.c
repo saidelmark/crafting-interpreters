728dcf14090e02ad772d293794201456be705738
Mark Saidelman <mark.saidelman@akvelon.com>
2023-05-23 22:40:12 +0200

    Add function objects to the VM

--- clox/vm.c
+++ clox/vm.c
@@ -13,6 +13,7 @@ VM vm;
 
 static void resetStack() {
 	vm.stackTop = vm.stack;
+	vm.frameCount = 0;
 }
 
 static void runtimeError(const char* format, ...) {
@@ -22,8 +23,9 @@ static void runtimeError(const char* format, ...) {
 	va_end(args);
 	fputs("\n", stderr);
 
-	size_t instruction = vm.ip - vm.chunk->code - 1;
-	int line = vm.chunk->lines[instruction];
+	CallFrame* frame = &vm.frames[vm.frameCount - 1];
+	size_t instruction = frame->ip - frame->function->chunk.code - 1;
+	int line = frame->function->chunk.lines[instruction];
 	fprintf(stderr, "[line %d] in script\n", line);
 	resetStack();
 }
@@ -74,10 +76,13 @@ static void concatenate() {
 }
 
 static InterpretResults run() {
-#define READ_BYTE() (*vm.ip++)
-#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+	CallFrame* frame = &vm.frames[vm.frameCount - 1];
+#define READ_BYTE() (*frame->ip++)
+#define READ_CONSTANT() \
+	(frame->function->chunk.constants.values[READ_BYTE()])
 #define READ_SHORT() \
-	(vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
+	(frame->ip += 2, \
+	(uint16_t)((frame->ip[-2] << 8) | frame->ip[-1]))
 #define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op) \
 	do { \
@@ -98,8 +103,8 @@ static InterpretResults run() {
 			printf(" ]");
 		}
 		printf("\n");
-		disassembleInstruction(vm.chunk,
-			(int) (vm.ip - vm.chunk->code));
+		disassembleInstruction(&frame->function->chunk,
+			(int) (frame->ip - frame->function->chunk.code));
 #endif
 		uint8_t instruction;
 		switch (instruction = READ_BYTE()) {
@@ -114,12 +119,12 @@ static InterpretResults run() {
 			case OP_POP: pop(); break;
 			case OP_GET_LOCAL: {
 				uint8_t slot = READ_BYTE();
-				push(vm.stack[slot]);
+				push(frame->slots[slot]);
 				break;
 			}
 			case OP_SET_LOCAL: {
 				uint8_t slot = READ_BYTE();
-				vm.stack[slot] = peek(0);
+				frame->slots[slot] = peek(0);
 				break;
 			}
 			case OP_GET_GLOBAL: {
@@ -189,17 +194,17 @@ static InterpretResults run() {
 			}
 			case OP_JUMP: {
 				uint16_t offset = READ_SHORT();
-				vm.ip += offset;
+				frame->ip += offset;
 				break;
 			}
 			case OP_JUMP_IF_FALSE: {
 				uint16_t offset = READ_SHORT();
-				if (isFalsey(peek(0))) vm.ip += offset;
+				if (isFalsey(peek(0))) frame->ip += offset;
 				break;
 			}
 			case OP_LOOP: {
 				uint16_t offset = READ_SHORT();
-				vm.ip -= offset;
+				frame->ip -= offset;
 				break;
 			}
 			case OP_DUP: push(peek(0)); break;
@@ -216,16 +221,13 @@ static InterpretResults run() {
 }
 
 InterpretResults interpret(const char* source) {
-	Chunk chunk;
-	initChunk(&chunk);
+	ObjFunction* function = compile(source);
+	if (function == NULL) return INTERPRET_COMPILE_ERROR;
 
-	if (!compile(source, &chunk)) {
-		freeChunk(&chunk);
-		return INTERPRET_COMPILE_ERROR;
-	}
-	vm.chunk = &chunk;
-	vm.ip = vm.chunk->code;
-	InterpretResults result = run();
-	freeChunk(&chunk);
-	return result;
+	push(OBJ_VAL(function));
+	CallFrame* frame = &vm.frames[vm.frameCount++];
+	frame->function = function;
+	frame->ip = function->chunk.code;
+	frame->slots = vm.stack;
+	return run();
 }
